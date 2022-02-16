a287692b40b31622a46bd28b2dfbb6174fbcbb3b
unknown <silobs.dev@gmail.com>
2022-02-17 03:42:55 +0530

    init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x02288feCcd7a15d621f9b420a1feB7054c68E977";
+// URL to extract Metadata for a Crypto Vamp NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
